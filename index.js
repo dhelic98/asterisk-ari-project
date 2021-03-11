8d2b6aa8df68085dfa0b1c3497915663c6225599
Dzenan Helic <dzenan.helic@gmail.com>
2021-03-11 15:31:52 +0100

    Added conference calls

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 var ari = require('ari-client');
 var util = require('util');
 
+let currentBridge = '';
+let bridgeType = '';
+
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
@@ -46,6 +49,7 @@ function clientLoaded(err, client) {
                 if (counter < 2) {
                     getExtension();
                 } else {
+
                     client.channels.create({
                         app: 'channel-dump',
                         endpoint: 'PJSIP/' + extensions[0]
@@ -57,7 +61,17 @@ function clientLoaded(err, client) {
                                 app: 'channel-dump',
                                 endpoint: 'PJSIP/' + extensions[1]
                             }, (err, channelDestin) => {
-                                channelDestin.dial({ channelId: channelOrig.id });
+                                var bridge = client.Bridge();
+                                bridge.create(function(err, bridge) {
+                                    client.channels.dial({ channelId: channelDestin.id });
+                                    client.channels.dial({ channelId: channelOrig.id });
+                                    bridge.addChannel({ channel: channelOrig.id });
+                                    bridge.addChannel({ channel: channelDestin.id });
+                                    currentBridge = bridge.id;
+                                    bridgeType = '0';
+                                });
+                                channelOrig.on('ChannelEnteredBridge', function(event, obj) {})
+                                channelDestin.on('ChannelEnteredBridge', function(event, obj) {})
                             });
                         }
                     });
@@ -69,18 +83,56 @@ function clientLoaded(err, client) {
 
 
     function createConference() {
+        let extensions = [];
+        let channels = [];
+        readline.question('Enter number of extensions?\n', number => {
+            number = parseInt(number);
+            let getExtension = () => {
+                readline.question('Enter extension\n', ext => {
+                    --number;
+                    extensions.push(ext);
+                    if (number > 0) {
+                        getExtension();
+                    } else {
+                        var bridge = client.Bridge();
+                        bridge.create({ type: 'mixing' }, function(err, bridge) {
+                            bridgeType = '1';
+                            extensions.forEach(ext => {
+                                client.channels.create({ app: 'channel-dump', endpoint: 'PJSIP/' + ext }, (err, channel) => {
+                                    channels.push(channel);
+                                    client.channels.dial({ channelId: channel.id });
+                                    bridge.addChannel({ channel: channel.id });
+                                })
+                            });
+                            currentBridge = bridge.id;
+                        });
+                    }
+                });
+            }
+            getExtension();
+        });
+
 
     }
 
     function joinConference() {
-
+        client.bridges.list(
+            function(err, bridges) {
+                let i = 1;
+                console.log("Available conferences");
+                bridges.forEach(b => {
+                    console.log(i++ + ") Conference")
+                    console.log("ID: " + b.id);
+                    console.log("Name: " + b.name)
+                    console.log("Creation time: " + b.creationtime);
+                    console.log('\n');
+                })
+            }
+        );
     }
 
     // handler for StasisStart event
     function stasisStart(event, channel) {
-        console.log(util.format(
-            'Channel %s has entered the application', channel.name));
-
         // use keys on event since channel will also contain channel operations
         Object.keys(event.channel).forEach(function(key) {
             console.log(util.format('%s: %s', key, JSON.stringify(channel[key])));
@@ -89,8 +141,29 @@ function clientLoaded(err, client) {
 
     // handler for StasisEnd event
     function stasisEnd(event, channel) {
-        console.log(util.format(
-            'Channel %s has left the application', channel.name));
+        client.bridges.get({ bridgeId: currentBridge }, function(err, bridge) {
+            if (bridge.channels.length == 0) {
+                bridge.destroy();
+            }
+            if (bridgeType == '0') {
+                if (bridge.channels.length < 2) {
+                    bridge.channels.forEach(mchannel => {
+                        client.bridges.removeChannel({ bridgeId: bridge.id, channel: mchannel }, function(err) {
+                            if (err) {
+                                throw err;
+                            }
+                        })
+                        client.channels.hangup({ channelId: mchannel },
+                            function(err) {
+                                if (err) {
+                                    throw err;
+                                }
+                            }
+                        );
+                    });
+                }
+            }
+        });
     }
 
     client.on('StasisStart', stasisStart);
